9bea66151661939d25966f7d90cdea83e0a05493
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-15 17:32:25 +0200

    create outline of the first section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Logo from './images/Logo.png';
+import Board from './images/Board.png';
+
+const Wrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+`;
+
+const Header = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #CDCEE6;
+`
+
+const HeaderLogo = styled.img`
+  height: 30px;
+  padding: 15px;
+`
+
+const TopContent = styled.div`
+  height: calc(100vh - 60px);
+  background-color: #CDCEE6;
+  padding: 0 10%;
+  display: grid;
+  align-content: center;
+  align-items: center;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const LogoImg = styled.img`
+  width: 80%;
+`
+
+const BoardImg = styled.img`
+  width: 100%;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header>
+        <HeaderLogo alt="Logo" src={Logo}></HeaderLogo>
+      </Header>
+      <TopContent>
+        <div>
+          <LogoImg alt="Logo" src={Logo}></LogoImg>
+          <p>
+            En liten nice slogan av något slag.
+          </p>
+        </div>
+        <BoardImg alt="The board" src={Board} />
+      </TopContent>
+    </Wrapper>
   );
 }
 
